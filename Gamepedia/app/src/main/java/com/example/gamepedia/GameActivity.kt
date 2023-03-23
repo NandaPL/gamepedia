2905e4389ccaae0b32a510b7d81185eedea3b756
NandaPL <fpl.snf19@uea.edu.br>
2023-03-23 11:19:21 -0400

    feat: home activity
    Styling home activity

--- Gamepedia/app/src/main/java/com/example/gamepedia/GameActivity.kt
+++ Gamepedia/app/src/main/java/com/example/gamepedia/GameActivity.kt
@@ -7,5 +7,7 @@ class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+
+
     }
 }
--- Gamepedia/app/src/main/java/com/example/gamepedia/MainActivity.kt
+++ Gamepedia/app/src/main/java/com/example/gamepedia/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.gamepedia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button_start)
+        button.setOnClickListener {
+            startActivity(Intent(this, GameActivity::class.java))
+            finish()
+        }
     }
 }
